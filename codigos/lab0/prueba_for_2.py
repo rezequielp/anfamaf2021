f096a6d94fc82f9513a421630f8a64e7671f77a0
ezequiel <rezequielp@gmail.com>
2021-07-04 07:58:37 -0300

    config files added commit test

--- codigos/lab0/prueba_for_2.py
+++ codigos/lab0/prueba_for_2.py
@@ -1,13 +1,15 @@
 x = int(input("ingrese un número: "))
 y = int(input("ingrese un número: "))
 
-def print_mat(x,y):
-	for idx in range(1,x+1):
-		for jdx in range(1,y+1):
-			if idx == jdx:
-				print(f"0.0",end=' ')
-			else:
-				print(f"{1/(idx+jdx):1.1f}",end=' ')
-		print('')
 
-print_mat(x,y)
+def print_mat(x, y):
+    for idx in range(1, x + 1):
+        for jdx in range(1, y + 1):
+            if idx == jdx:
+                print(f"0.0", end=' ')
+            else:
+                print(f"{10 / (idx + jdx):1.3f}", end=' ')
+        print('')
+
+
+print_mat(x, y)
